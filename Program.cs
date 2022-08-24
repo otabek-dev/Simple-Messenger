26066691c765cb972b63d4f86930b05a3b2d4277
Otabek Rustamov <otabek.pro@hotmail.com>
2022-08-24 11:04:17 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Simple_Messenger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
